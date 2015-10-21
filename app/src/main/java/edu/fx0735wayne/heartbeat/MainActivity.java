730004ac32ade1b8d4f0ae5f183a8d47b17ea09f
sichen <sichenjiajia@gmail.com>
2015-10-21 00:04:40 -0400

    Main加入实时获取分贝值

--- app/src/main/java/edu/fx0735wayne/heartbeat/MainActivity.java
+++ app/src/main/java/edu/fx0735wayne/heartbeat/MainActivity.java
@@ -1,25 +1,23 @@
 package edu.fx0735wayne.heartbeat;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -37,11 +35,12 @@ public class MainActivity extends AppCompatActivity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
-
     private static final String LOG_TAG = "AudioRecordTest";
     //语音文件保存路径
     static String FileName = null;
-
+    static double[] x = new double[50];
+    static TextView result;
+    public static double[] res = new double[50];
 
 
     //语音操作对象
@@ -62,8 +61,105 @@ public class MainActivity extends AppCompatActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+        Print p = new Print();
+        p.start();
+
+    }
+
+    //实时输出分贝值
+    static class MyAudioRecorder  implements View.OnClickListener {
+        static final String TAG = "AudioRecord";
+        static final int SAMPLE_RATE_IN_HZ = 8000;
+
+        static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ,
+                AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+        AudioRecord mAudioRecord;
+        boolean isGetVoiceRun;
+        Object mLock;
+
+        public MyAudioRecorder() {
+            mLock = new Object();
+            //result = (TextView)rootView.findViewById(R.id.resultTest);
+        }
+
+        @Override
+        public void onClick(View v) {
 
 
+            if (isGetVoiceRun) {
+                Log.e(TAG, "还在录着呢");
+                return;
+            }
+            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
+            if (mAudioRecord == null) {
+                Log.e("sound", "mAudioRecord初始化失败");
+            }
+            isGetVoiceRun = true;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    mAudioRecord.startRecording();
+                    short[] buffer = new short[BUFFER_SIZE];
+                    int j =0;
+                    while (isGetVoiceRun) {
+                        //r是实际读取的数据长度，一般而言r会小于buffersize
+                        int r = mAudioRecord.read(buffer, 0, BUFFER_SIZE);
+                        long v = 0;
+                        // 将 buffer 内容取出，进行平方和运算
+                        for (int i = 0; i < buffer.length; i++) {
+                            v += buffer[i] * buffer[i];
+                        }
+                        // 平方和除以数据总长度，得到音量大小。
+                        double mean = v / (double) r;
+                        double volume = 10 * Math.log10(mean);
+                        Log.d(TAG, "分贝值:" + volume);
+                        res[j] = volume;
+                        j++;
+                        if(j>=50){
+                            System.arraycopy(res,0,x,0,50);
+                            j = 0;
+                            notify();
+                        }
+                        // 大概一秒五十次
+                        synchronized (mLock) {
+                            try {
+                                mLock.wait(20);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    mAudioRecord.stop();
+                    mAudioRecord.release();
+                    mAudioRecord = null;
+                }
+            }).start();
+        }
+    }
+
+    //实时展示数据线程类
+    static public class Print extends Thread{
+        @Override
+        public void run(){
+            int i = 0;
+            boolean isprinting =true;
+            while (isprinting){
+                try{
+                    wait();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                while(i<50){
+                    Log.i("测试-" , "分贝值:" + x[i]);
+                    Log.d("测试-" , "分贝值:" + x[i]);
+                    i++;
+                }
+
+            }
+        }
     }
 
     //开始录音
@@ -218,6 +314,7 @@ public class MainActivity extends AppCompatActivity
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
             //界面控件
+            TextView result;
             Button startRecord;
             Button startPlay;
             Button stopRecord;
@@ -226,14 +323,17 @@ public class MainActivity extends AppCompatActivity
             startRecord = (Button)rootView.findViewById(R.id.startRecord);
             startRecord.setText("Start Record");
             //绑定监听器
-            startRecord.setOnClickListener(new startRecordListener());
+            startRecord.setOnClickListener(new MyAudioRecorder());
 
             //结束录音
             stopRecord = (Button)rootView.findViewById(R.id.stopRecord);
             stopRecord.setText("Stop Record");
             stopRecord.setOnClickListener(new stopRecordListener());
+            //显示结果
+            result = (TextView)rootView.findViewById(R.id.resultTest);
 
-            //开始播放
+
+                //开始播放
             startPlay = (Button)rootView.findViewById(R.id.startPlay);
             startPlay.setText("Start Play");
             //绑定监听器
